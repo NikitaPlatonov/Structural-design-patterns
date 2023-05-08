b97398e171d7f1261a67bcfceba9c5f207ecd348
Nikita Platonov <fifaakkaynt92@gmail.com>
2023-05-08 09:10:21 +0300

    init

---
+++ src/main/java/ru/netology/IntsCalculator.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+public class IntsCalculator implements Ints {
+    private final Calculator target;
+
+    public IntsCalculator(Calculator target) {
+        this.target = target;
+    }
+    @Override
+    public int sum(int arg0, int arg1) {
+        return (int) target.newFormula().addOperand(arg0).addOperand(arg1).calculate(Calculator.Operation.SUM).result();
+    }
+}
