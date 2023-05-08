13166825477fd8452fe15dda96f4cdffda1ed037
Nikita Platonov <fifaakkaynt92@gmail.com>
2023-05-08 09:42:06 +0300

    Продемонстрировал работу класса-фасада BinOps в Main

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -2,13 +2,18 @@ package ru.netology;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        IntsCalculator intsCalculator = new IntsCalculator(calculator);
-        System.out.println("Результат возведения числа в степень");
-        System.out.println(intsCalculator.pow(5,2));
-        System.out.println("Результат сложения");
-        System.out.println(intsCalculator.sum(100,150));
-        System.out.println("Результат умножения");
-        System.out.println(intsCalculator.mult(10,5));
+        BinOps binOps = new BinOps();
+        // "1010" - 10
+        String resultSum = (binOps.sum("1010", "1010"));
+        System.out.println("Результат сложения в двоичной системе счисления");
+        System.out.println(resultSum);
+        System.out.println("Результат сложения в десятичной системе счисления");
+        System.out.println(Integer.parseInt(resultSum, 2));
+        System.out.println();
+        String resultMult = binOps.mult("1010", "1010");
+        System.out.println("Результат умножения в двоичной системе счисления");
+        System.out.println(resultMult);
+        System.out.println("Результат умножения в десятичной системе счисления");
+        System.out.println(Integer.parseInt(resultMult , 2));
     }
 }
