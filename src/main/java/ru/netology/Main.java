bb738a45f4157516995acb8e07d5be5e47656b71
Nikita Platonov <fifaakkaynt92@gmail.com>
2023-05-08 09:18:44 +0300

    Продемонстрировал работу своего класс-адаптера в Main

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -4,6 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         IntsCalculator intsCalculator = new IntsCalculator(calculator);
+        System.out.println("Результат возведения числа в степень");
         System.out.println(intsCalculator.pow(5,2));
+        System.out.println("Результат сложения");
+        System.out.println(intsCalculator.sum(100,150));
+        System.out.println("Результат умножения");
+        System.out.println(intsCalculator.mult(10,5));
     }
 }
